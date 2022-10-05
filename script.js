4fb2406f0102217b0f3c7c3f879e714fef2e4bcc
Emmanuel-Obiora <emmanuelobiora94@gmail.com>
2022-10-05 09:40:44 +0100

    made correction to time switch

--- script.js
+++ script.js
@@ -12,27 +12,24 @@ function displayTime(){
     let img4 = document.getElementById('sunrise');
 
 
-    if (hrs >= 12 && hrs <= 15){
-        timing.innerHTML = "GOOD AFTERNOON, IT'S CURRENTLY";
-        img3.style.visibility = 'visible';
+    if (hrs >= 6 && hrs <= 11){
+        document.body.style.backgroundImage = "url('img/sunrise-bg.jpg')";
+        img4.style.visibility = 'visible';
+    }
+    else if(hrs >= 12 && hrs <= 15){
         document.body.style.backgroundImage = "url('img/bright-forest.jpg')";
+        img3.style.visibility = 'visible';
+
+        
     }
     else if(hrs >= 16 && hrs <= 19){
-        timing.innerHTML = "GOOD EVENING, IT'S CURRENTLY";
-        img1.style.visibility = 'visible';
         document.body.style.backgroundImage = "url('img/sunset.jpg')";
-    }
-    else if(hrs >= 20 && hrs <= 23){
-        timing.innerHTML = "GOOD EVENING, IT'S CURRENTLY";
-        img2.style.visibility = 'visible';
-        document.body.style.backgroundImage = "url('img/darkForest.jpg')";
-        document.body.style.backgroundColor = 'black';
+        img1.style.visibility = 'visible';
     }
     else{
-        timing.innerHTML = "GOOD MORNING, IT'S CURRENTLY";
-        img4.style.visibility = 'visible';
-        document.body.style.backgroundImage = "url('img/sunrise-bg.jpg')";
-        
+        document.body.style.backgroundImage = "url('img/darkForest.jpg')";
+        document.body.style.backgroundColor = 'black';
+        img2.style.visibility = 'visible';
     }
     
     // The code below helps to output O for digits less than 10
@@ -42,6 +39,12 @@ function displayTime(){
     if (min <= 9 ){
         min ="0" + min;
     }
+
+
+    let dayChange = (hrs >= 0 && hrs < 12)? timing.innerHTML = "GOOD MORNING, IT'S CURRENTLY":
+    (hrs > 11 && hrs < 16)? timing.innerHTML = "GOOD AFTERNOON, IT'S CURRENTLY":
+    (hrs > 15 && hrs < 20)? timing.innerHTML = "GOOD EVENING, IT'S CURRENTLY":
+    timing.innerHTML = "GOOD EVENING, IT'S CURRENTLY";
 
     document.getElementById('hours').innerHTML = hrs;
     document.getElementById('minutes').innerHTML = min;
